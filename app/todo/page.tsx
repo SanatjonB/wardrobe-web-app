bb4f75c89c1ef8715aa9267293de84e784ec914b
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2026-02-25 00:50:58 -0500

    cleaned up the nextjs and supabase layout worked on a rough design idea for layout The theme switcher still works black or white or system deleted the tutorial added another css page.css got the supabase linked

--- app/todo/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@/lib/supabase/server";
-
-export default async function Page() {
-  const supabase = await createClient();
-  const { data: Todo } = await supabase.from("Todo").select();
-
-  return <pre>{JSON.stringify(Todo, null, 2)}</pre>;
-}
